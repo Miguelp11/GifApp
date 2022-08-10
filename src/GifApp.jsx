6999d9148aebf211d5a47d787b1fa45d18c8eec8
Miguel Ponce <miguel_p1194@hotmail.com>
2022-08-10 18:00:00 -0500

    creacion del componente AddCategory y logica de la misma

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
 export const GifApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+  
+  const onAddCategory = () => {
+    setCategories(['Nuevo', ...categories]);
+  }
+
   return (
     <>
+      {/* Titulo */}
       <h1>GifApp</h1>
+
+      {/* Input */}
+      <AddCategory AddCategory={setCategories}/>
+
+      {/* Listado de Gifs */}
+      
+      <ol>
+        {categories.map(category => <li key={category}>{category}</li>)}
+      </ol>
+        {/* Gifs Items */}
     </>
-  )
+  );
 }
